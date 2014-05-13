49d56fcf3a8ed59bbab90119f2b6da8e3aee169e
Daniel <dariso23@gmail.com>
2014-05-13 10:25:40 -0600

    Agregada pagina home

--- Monchie_app/config/routes.rb
+++ Monchie_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :ordens
 
   resources :restaurantes
@@ -7,6 +13,8 @@ Rails.application.routes.draw do
   resources :productos
 
   resources :usuario_clientes
+  
+  resources :menus
   
 
   # The priority is based upon order of creation: first created -> highest priority.
