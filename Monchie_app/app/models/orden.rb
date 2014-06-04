cfd9edffd3a98c35cfcb5be4a8b3e743b3b2a388
kalita09 <kalam_1992@hotmail.com>
2014-06-04 14:59:42 -0600

    relacion orden

--- Monchie_app/app/models/orden.rb
+++ Monchie_app/app/models/orden.rb
@@ -1,3 +1,4 @@
 class Orden < ActiveRecord::Base
-
+has_one :usuario_cliente
+has_many :productos_vendidos
 end
--- Monchie_app/db/schema.rb
+++ Monchie_app/db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -75,4 +74,3 @@ ActiveRecord::Schema.define(version: 20140604082924) do
   end
 
 end
-
