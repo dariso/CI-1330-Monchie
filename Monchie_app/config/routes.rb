c2ef49fef1ed4b87ae95f9538d905a4a46fdd291
Elexia12 <xiannie.rivas@gmail.com>
2014-06-07 14:50:39 -0600

    Agregados estilos, navebar, sidebar, etc

--- Monchie_app/config/routes.rb
+++ Monchie_app/config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   resources :restaurantes
   resources :usuario_clientes
   resources :productos_vendidos
-  root 'static_pages#home'
+  root :to => "static_pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
