3996dd02b2b499a1ab516c3c9bb08128ee1d5098
Daniel <dariso23@gmail.com>
2014-06-09 09:07:27 -0600

    Agregado columna modo retiro a ordens

--- Monchie_app/db/schema.rb
+++ Monchie_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609073906) do
+ActiveRecord::Schema.define(version: 20140609150528) do
 
   create_table "cuenta_usuarios", force: true do |t|
     t.string   "usuario"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140609073906) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "usuario_cliente_id"
+    t.string   "modoretiro"
   end
 
   create_table "productos", force: true do |t|
