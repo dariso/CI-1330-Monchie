2647b917c5304c531a359ab28b90139f5936660b
Meli Garro <melissagm08@gmail.com>
2014-06-11 00:41:22 -0600

    cambio email por usuario

--- Monchie_app/db/schema.rb
+++ Monchie_app/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140611025043) do
-
 
   create_table "cuenta_usuarios", force: true do |t|
     t.string   "usuario"
