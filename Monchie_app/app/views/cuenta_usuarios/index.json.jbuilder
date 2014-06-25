0b69e05e9f503cd8d4ec6ce938b1bbf0b28cb511
Daniel <dariso23@gmail.com>
2014-06-24 22:50:46 -0600

    Borrada tabla cuenta_usuarios,cambios por deployment a heroku

--- Monchie_app/app/views/cuenta_usuarios/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@cuenta_usuarios) do |cuenta_usuario|
-  json.extract! cuenta_usuario, :id, :usuario, :password
-  json.url cuenta_usuario_url(cuenta_usuario, format: :json)
-end
--- Monchie_app/db/schema.rb
+++ Monchie_app/db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622174625) do
-
-  create_table "cuenta_usuarios", force: true do |t|
-    t.string   "usuario"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140625044353) do
 
   create_table "emails", force: true do |t|
     t.string   "email"
